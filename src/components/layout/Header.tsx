c52c8f0ac1562325a16d1207958e64383420efe9
Simon-Honore <simon.honore6@gmail.com>
2023-12-03 23:58:56 +0100

    feat: eslint tailwind plugin

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <Image src={SiteConfig.logo} width={50} height={35} alt="app logo" />
           <Typography variant="h3" as={Link} href="/">
             {SiteConfig.title}
